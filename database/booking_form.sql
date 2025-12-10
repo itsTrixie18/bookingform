69deea60cf71b57a4b1c581523581007d23798f1
Trixie Mifrano <trca.mifrano.coc@phinmaed.com>
2025-12-11 07:45:12 +0800

    new update

--- database/booking_form.sql
+++ database/booking_form.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2024 at 02:28 AM
+-- Generation Time: Dec 11, 2025 at 12:44 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,7 +31,7 @@ CREATE TABLE `booking_form` (
   `user_id` int(11) NOT NULL,
   `fullname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `contact_no` int(11) NOT NULL,
+  `contact_no` varchar(11) NOT NULL,
   `address` varchar(255) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -41,7 +41,11 @@ CREATE TABLE `booking_form` (
 --
 
 INSERT INTO `booking_form` (`user_id`, `fullname`, `email`, `contact_no`, `address`, `date`) VALUES
-(1, 'Trixie', 'trixdvmps@gmail.com', 2147483647, 'Damilag', '2024-12-01');
+(1, 'Trixie', 'trixdvmps@gmail.com', '2147483647', 'Damilag', '2024-12-01'),
+(2, 'Kenji Lumayog', 'kesa.lumayog.coc@phinmaed.com', '2147483647', 'Lapasan', '2024-12-03'),
+(3, 'Trixie Mifrano', 'trixdumpling@gmail.com', '2147483647', 'Damilag', '2024-12-25'),
+(4, 'Sara Smith', 'Sara@gmail.com', '2147483647', 'Cagayan', '2025-12-12'),
+(5, 'Kaia Lei', 'Kaia@gmail.com', '09086754382', 'Manolo', '2025-12-12');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +65,7 @@ ALTER TABLE `booking_form`
 -- AUTO_INCREMENT for table `booking_form`
 --
 ALTER TABLE `booking_form`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
